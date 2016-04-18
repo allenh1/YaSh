ad5d7df3c76c6e523a798c1d6d2b7886f6a93170
allenh1 <allenh1@e80aa85c-8b93-4d9b-b8ec-092e4ac18bd2>
2016-04-18 15:05:19 +0000

    Adding more to autogen script.

--- repos/hsh/autogen.sh
+++ repos/hsh/autogen.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 # This script sets up the build.
-autoreconf --force --install -I config -I m4
 libtoolize --install
-
+aclocal
+automake --add-missing
+autoreconf --force --install -I config -I m4
+autoconf
 # Done setting things up.
 echo "You may now run the configure script."
 
