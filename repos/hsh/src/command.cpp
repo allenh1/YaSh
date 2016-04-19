c1dc99369b54ad48027dde0feba0ab1872efa95a
allenh1 <allenh1@e80aa85c-8b93-4d9b-b8ec-092e4ac18bd2>
2016-04-19 23:47:56 +0000

    Tab completion!

--- repos/hsh/src/command.cpp
+++ repos/hsh/src/command.cpp
@@ -516,31 +516,31 @@ void sigchld_handler(int signum)
   } errno = saved_errno;
 }
 
-  int main()
-  {
-    struct sigaction ctrl_action;
-    ctrl_action.sa_handler = ctrlc_handler;
-    sigemptyset(&ctrl_action.sa_mask);
-    ctrl_action.sa_flags = SA_RESTART;
+int main()
+{
+  struct sigaction ctrl_action;
+  ctrl_action.sa_handler = ctrlc_handler;
+  sigemptyset(&ctrl_action.sa_mask);
+  ctrl_action.sa_flags = SA_RESTART;
 
-    if (sigaction(SIGINT, &ctrl_action, NULL)) {
-      perror("sigint");
-      _exit(6);
-    }
-
-    struct sigaction chld;
-    chld.sa_handler = sigchld_handler;
-    sigemptyset(&chld.sa_mask);
-    chld.sa_flags   = SA_RESTART | SA_NOCLDSTOP;
-
-    if (sigaction(SIGCHLD, &chld, NULL) == -1) {
-      perror("sigchild");
-      _exit(7);
-    } 
-    Command::currentCommand.prompt();
-    yyparse();
-
-    return 0;
+  if (sigaction(SIGINT, &ctrl_action, NULL)) {
+    perror("sigint");
+    _exit(6);
   }
+
+  struct sigaction chld;
+  chld.sa_handler = sigchld_handler;
+  sigemptyset(&chld.sa_mask);
+  chld.sa_flags   = SA_RESTART | SA_NOCLDSTOP;
+
+  if (sigaction(SIGCHLD, &chld, NULL) == -1) {
+    perror("sigchild");
+    _exit(7);
+  } 
+  Command::currentCommand.prompt();
+  yyparse();
+
+  return 0;
+}
 
 
