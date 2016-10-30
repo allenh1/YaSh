3767497886741d3492e23938e03af27112759f67
Hunter L. Allen <allen286@purdue.edu>
2016-10-30 18:32:14 -0400

    Removed unnecessary sort call from wildcard, as well as made the bash locale match that of our sort.

--- src/wildcard.cpp
+++ src/wildcard.cpp
@@ -1,31 +1,29 @@
 #include "wildcard.hpp"
 
 void wildcard_expand(char * arg) {
-  // return if arg does not contain * or ?
-  if(!strchr(arg, '*') && !strchr(arg, '?')) {
-    Command::currentSimpleCommand->insertArgument(arg);
-    return;
-  }
+	// return if arg does not contain * or ?
+	if(!strchr(arg, '*') && !strchr(arg, '?')) {
+		Command::currentSimpleCommand->insertArgument(arg);
+		return;
+	}
   
-  bool hidden = false;
+	bool hidden = false;
 
-  char * a = arg;
+	char * a = arg;
   
-  for (int i=0;*(a+1);a++, i++) {
-    if ((*a == '/' && *(a+1) == '.' && *(a+1) == '*')
-       || (i==0 && *(a)=='.' && *(a+1)=='*')) {
-      hidden = true;
-    }
-  }
+	for (int i=0;*(a+1);a++, i++) {
+		if ((*a == '/' && *(a+1) == '.' && *(a+1) == '*')
+			|| (i==0 && *(a)=='.' && *(a+1)=='*')) {
+			hidden = true;
+		}
+	}
  
-  glob_t results;
-  if(hidden) {
-    glob((const char*)arg, GLOB_PERIOD, (int)NULL, &results);
-  } else { glob((const char*)arg, GLOB_ERR,(int)NULL, &results); }
+	glob_t results;
+	if(hidden) {
+		glob((const char*)arg, GLOB_PERIOD, (int)NULL, &results);
+	} else { glob((const char*)arg, GLOB_ERR,(int)NULL, &results); }
 
-  for(int i=0; i < results.gl_pathc; i++) {
-    Command::currentCommand.wc_collector.push_back(results.gl_pathv[i]);
-  } std::sort(Command::currentCommand.wc_collector.begin(),
-	      Command::currentCommand.wc_collector.end());
-  
+ 	for(int i=0; i < results.gl_pathc; i++) {
+		Command::currentCommand.wc_collector.push_back(results.gl_pathv[i]);
+	}  
 }
